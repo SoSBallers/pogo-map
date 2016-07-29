ad670b0d1796ee0d52cea1672cdf0c025b87c70e
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-07-29 15:50:55 -0400

    Fixed problem where Pokemon Go blocks requests

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 F=(`cat ignore/pogoauth.txt`)
-sudo python /home/ubuntu/pogo-map/runserver.py -u ${F[0]} -p ${F[1]} -l "35.787072,-78.684737" -st 15 -H 0.0.0.0 -P 80 -t 15 &
+sudo python /home/ubuntu/pogo-map/runserver.py -u ${F[0]} -p ${F[1]} -l "35.787072,-78.684737" -st 15 -H 0.0.0.0 -P 80 -t 3 &
